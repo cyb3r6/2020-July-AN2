e42d77b8daf0be8b20618a40a85c60286d7a0bf6
nakisa <nakisa.donnelly@gmail.com>
2020-08-24 17:20:22 -0600

    Session 9
    - Animating our SimHand.
    - Collecting information from our controllers in our VRInput script.
    - Starting to animate our VR Hands.

---
+++ 2020 July AN 2/Assets/Scripts/SimHandAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimHandAnimator : MonoBehaviour
+{
+    private Animator simHandAnim;
+
+    void Start()
+    {
+        simHandAnim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            simHandAnim.SetBool("IsClosing", true);     // closing the fist
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            simHandAnim.SetBool("IsClosing", false);    // opening the fist
+        }
+    }
+}
---
+++ 2020 July AN 2/Assets/Scripts/VRInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRInput : MonoBehaviour
+{
+    public bool isLeftHand;     // if true, left controller
+    public float gripValue;
+
+    private string gripAxis;    
+    
+    void Awake()
+    {
+        if (isLeftHand)
+        {
+            gripAxis = "LeftGrip";
+        }
+        else
+        {
+            gripAxis = "RightGrip";
+        }
+    }
+
+    
+    void Update()
+    {
+        gripValue = Input.GetAxis(gripAxis);
+    }
+}
